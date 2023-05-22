e8e4ca1daaa5b856458e9897ece34feb5731191b
Mehdi Imeni <imeni1982@gmail.com>
2023-05-22 11:28:45 +0330

    api headers

--- icore/apis/iweb/global/website_alert.php
+++ icore/apis/iweb/global/website_alert.php
@@ -1,4 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include dirname(__FILE__, 4) . "/iassets/include/DBLoader.php";
 
