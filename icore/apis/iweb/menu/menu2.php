db6d721d5c2140e101911738e8f90949066ab8e8
Mehdi Imeni <imeni1982@gmail.com>
2023-05-22 11:50:59 +0330

    api menu2

--- icore/apis/iweb/menu/menu2.php
+++ icore/apis/iweb/menu/menu2.php
@@ -7,7 +7,17 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 include dirname(__FILE__, 4) . "/iassets/include/DBLoader.php";
 
-$gender = strtolower($_POST['gender']);
-$category = $_POST['category'];
-$GroupIdKey = @$objORM->Fetch( " Enabled = 1 and Name = '$category' ", "IdKey" , TableIWNewMenu2)->IdKey;
+$condition = " Enabled = 1  ";
+
+if (isset($_POST['gender'])) {
+    $gender = strtolower($_POST['gender']);
+}
+
+
+if (isset($_POST['category'])) {
+    $category= $_POST['category'];
+    $condition = " Enabled = 1 and Name = '$category' ";
+}
+
+$GroupIdKey = @$objORM->Fetch( $condition, "IdKey" , TableIWNewMenu2)->IdKey;
 echo @$objORM->FetchJson(TableIWNewMenu3, " Enabled = 1 and GroupIdKey = '$GroupIdKey' ", 'Name,LocalName');
