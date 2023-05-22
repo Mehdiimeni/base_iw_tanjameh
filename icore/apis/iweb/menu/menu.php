b027bcf61f84359bb0e265853a0d089d804bd503
Mehdi Imeni <imeni1982@gmail.com>
2023-05-22 11:43:33 +0330

    api menu

--- icore/apis/iweb/menu/menu.php
+++ icore/apis/iweb/menu/menu.php
@@ -7,7 +7,12 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 include dirname(__FILE__, 4) . "/iassets/include/DBLoader.php";
 
-$gender = strtolower($_POST['gender']);
-$gender != '' ? $condition = " Enabled = 1 and Name = '$gender' " : $condition = " Enabled = 1  " ;
-$GroupIdKey = @$objORM->Fetch( " Enabled = 1 and Name = '$gender' ", "IdKey" , TableIWNewMenu)->IdKey;
+$condition = " Enabled = 1  ";
+
+if (isset($_POST['gender'])) {
+    $gender = strtolower($_POST['gender']);
+    $condition = " Enabled = 1 and Name = '$gender' ";
+}
+
+$GroupIdKey = @$objORM->Fetch($condition, "IdKey", TableIWNewMenu)->IdKey;
 echo @$objORM->FetchJson(TableIWNewMenu2, " Enabled = 1 and GroupIdKey = '$GroupIdKey' ", 'Name,LocalName');
