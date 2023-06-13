50a68b6dfcc05a0747ee045eba51ec74203b998f
Mehdi Imeni <imeni1982@gmail.com>
2023-06-13 18:14:40 +0330

    change evry think 2

--- iweb/page/gender.php
+++ iweb/page/gender.php
@@ -5,18 +5,18 @@ if ($_GET['gender'] == strtolower('men'))
      $_SESSION['page_name_system'] = 'MenFirstPage';
 
 
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'temp', 'top');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'global', 'top');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'global', 'nav');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'global', 'menu');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'global', 'mobile_view');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'adver', 'banner_adver_1', '0');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'adver', 'banner_adver_2', '0');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'adver', 'user_trase', '0');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'adver', 'banner_adver_3', '0');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'adver', 'user_like', '0');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'adver', 'brand_box', '0');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'adver', 'trend_categories', '0');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'global', 'newsletter', '0');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'global', 'footer');
-(new FileCaller)->FileIncluderWithControler('./iweb/', 'temp', 'down');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'temp', 'top');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'global', 'top');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'global', 'nav');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'global', 'menu');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'global', 'mobile_view');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'adver', 'banner_adver_1', '0');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'adver', 'banner_adver_2', '0');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'adver', 'user_trase', '0');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'adver', 'banner_adver_3', '0');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'adver', 'user_like', '0');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'adver', 'brand_box', '0');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'adver', 'trend_categories', '0');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'global', 'newsletter', '0');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'global', 'footer');
+(new FileCaller)->FileIncluderWithControler('./iweb', 'temp', 'down');
